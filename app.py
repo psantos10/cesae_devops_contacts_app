9468f3d43051b36b1bbba25be3d5586f51f4163e
Patrício dos Santos <hello@psantos.dev>
2025-05-12 20:05:44 +0100

    Adicionando novas rotas

--- app.py
+++ app.py
@@ -6,6 +6,23 @@ app = Flask(__name__)
 def index():
   return render_template("index.html")
 
+@app.route("/adicionar")
+def add():
+  return "<h1>Adicionar Contacto</h1>"
+
+@app.route("/adicionar", methods=["POST"])
+def save():
+  return "<h1>Contacto Adicionado</h1>"
+
+@app.route("/editar")
+def edit():
+  return "<h1>Editar Contacto</h1>"
+
+@app.route("/editar", methods=["POST"])
+def update():
+  return "<h1>Contacto Editado</h1>"
+
+
 if __name__ == '__main__':
   app.run(debug=True)
 
