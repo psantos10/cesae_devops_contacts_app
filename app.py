6d1b1b0b06d2eeb03190ef3f6d1f34582b9237ab
Patrício dos Santos <hello@psantos.dev>
2025-05-16 15:31:48 +0100

    Use postgresql

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///.development.db'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///.development.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/cesae_contacts_app'
 
     db.init_app(app)
 
