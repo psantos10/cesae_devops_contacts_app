e59484734ad8591d97896a655c7d162e39d6d6e8
Patrício dos Santos <hello@psantos.dev>
2025-05-19 22:44:11 +0100

    Deployment Kubernetes

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///.development.db'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:postgres@{os.environ.get("DB_HOST")}:5432/cesae_contacts_app'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///.development.db'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:postgres@{os.environ.get("DB_HOST")}:5432/cesae_contacts_app'
 
     db.init_app(app)
 
